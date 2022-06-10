73c9930a29a55ff0079e8792fcd4acd13358634c
Tamara Volcharenko <vtvo16@gmail.com>
2022-06-10 09:00:55 +0300

    Fixed lab 2

--- lab2/src/Main.hs
+++ lab2/src/Main.hs
@@ -1,5 +1,24 @@
 module Main where
 
+picks :: [Int] -> [(Int, Int)]
+picks list =
+  filter condition
+    . zip list
+    $ [1 .. length list]
+  where
+    condition (x, i) = prev > x && next > x
+      where
+        prev = get (i - 2) list
+        next = get i list
+
+get :: Int -> [a] -> a
+get n list
+  | n > 0 = list !! rem n (length list)
+  | otherwise = list !! rem (n + length list) (length list)
+
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  --               .           .        .          .
+  print . picks $ [0, 1, 1, 2, 1, 3, 4, 1, 5, 19, 10, 13, 9]
+
+--                 1  2  3  4  5  6  7  8  9  10  11  12 13
